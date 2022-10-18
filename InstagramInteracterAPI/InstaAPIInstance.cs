5aef9c393c6262e6a7d186a58c4efc121a1538ea
Kappaseero <casperkoponen2@gmail.com>
2022-10-18 13:18:23 +0300

    added IInstaApi instance creation

---
+++ InstagramInteracterAPI/InstaAPIInstance.cs
@@ -0,0 +1,25 @@
+﻿using InstagramApiSharp;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Classes.SessionHandlers;
+using InstagramApiSharp.API;
+
+namespace InstagramInteracterAPI
+{
+    
+
+    public static class InstaAPIInstance
+    {
+        
+        public static IInstaApi CreateInstance (IHttpClientFactory factory, string username, string password)
+        {
+            IInstaApi api = InstaApiBuilder.CreateBuilder()
+                .SetUser(UserSessionData.ForUsername(username).WithPassword(password))
+                .UseHttpClient(factory.CreateClient())
+                .Build();
+
+            return api;
+        }
+    }
+}
