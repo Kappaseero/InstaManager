872e4be0c6e01d02aaabe3ed07ecb2938f287c0f
Kappaseero <casperkoponen2@gmail.com>
2022-10-24 12:43:47 +0300

    Added different kinds of post classes and an IPost interface for them

---
+++ InstagramPrivateAPI/InstagramInteracter/Posts/IPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.InstagramInteracter.Posts
+{
+    internal interface IPost
+    {
+        internal string GetText();
+        internal 
+       
+
+    }
+}
