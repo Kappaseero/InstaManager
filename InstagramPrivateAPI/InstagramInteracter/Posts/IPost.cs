44ecbf190b76606db1a0cd659e6fa4b3d9c6f746
Kappaseero <casperkoponen2@gmail.com>
2022-10-25 10:28:36 +0300

    Updated IPost interface to match with InstagramAPISharp UploadPhotoAsync()

--- InstagramPrivateAPI/InstagramInteracter/Posts/IPost.cs
+++ InstagramPrivateAPI/InstagramInteracter/Posts/IPost.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InstagramApiSharp;
+using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Classes.Models;
 
 namespace InstagramPrivateAPI.InstagramInteracter.Posts
 {
     internal interface IPost
     {
-        internal string GetText();
-        internal 
-       
+        internal string Caption { get; set; }
+        internal InstaImageUpload Image { get; set; }
 
     }
 }
