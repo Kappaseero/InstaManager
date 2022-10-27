06c705aff7aee1030095de18c3f5971ec3ffed87
Kappaseero <casperkoponen2@gmail.com>
2022-10-27 15:32:53 +0300

    Added QuestionModel for future Quizzes and the like

---
+++ InstagramPrivateAPI/Models/QuestionAnswerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Models
+{
+    internal class QuestionAnswerModel
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        internal QuestionAnswerModel(int id, string question, string answer)
+        {
+            Id = id;
+            Question = question;
+            Answer = answer;
+        }
+    }
+}
