d64b9360415f3e2b25b073e91217b730ffb37967
Kappaseero <casperkoponen2@gmail.com>
2022-10-29 15:08:27 +0300

    Models for  the leaderboard

---
+++ InstagramPrivateAPI/Models/LeaderboardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Models
+{
+    internal class LeaderboardModel
+    {
+        internal LeaderboardModel(SortedList<int, LeaderboardPersonModel> sortedList)
+        {
+            LeaderboardList = sortedList;
+        }
+        public SortedList<int, LeaderboardPersonModel> LeaderboardList { get; set; }
+    }
+}
---
+++ InstagramPrivateAPI/Models/LeaderboardPersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Models
+{
+    internal class LeaderboardPersonModel
+    {
+        internal LeaderboardPersonModel(string name)
+        {
+            Name = name;
+            Score = 0;
+        }
+        public int Score { get; set; }
+        public string Name { get; set; }
+    }
+}
