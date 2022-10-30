61882b3194e441bb6d9e5253c2af3a63284114d7
Kappaseero <casperkoponen2@gmail.com>
2022-10-30 14:57:47 +0200

    A LeaderboardModel for the LeaderboardPost

--- InstagramPrivateAPI/InstagramInteracter/Posts/LeaderboardPost.cs
+++ InstagramPrivateAPI/InstagramInteracter/Posts/LeaderboardPost.cs
@@ -4,22 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InstagramPrivateAPI.Models;
 
 namespace InstagramPrivateAPI.InstagramInteracter.Posts
 {
     internal class LeaderboardPost : PhotoPost
     {
-        public LeaderboardPost(HttpClient httpClient, InstaImageUpload instaImageUpload)
+        public LeaderboardPost(HttpClient httpClient, InstaImageUpload instaImageUpload, LeaderboardModel leaderboardModel)
         {
             Client = httpClient;
             Caption = GenerateCaption();
             Image = instaImageUpload;
             Image.Uri = GenerateMedia();
+            Leaderboard = leaderboardModel;
         }
 
         internal override HttpClient Client { get; set; }
         internal override string Caption { get; set; }
         internal override InstaImageUpload Image { get; set; }
+        public LeaderboardModel Leaderboard { get; set; }
 
         private string GenerateCaption()
         {
