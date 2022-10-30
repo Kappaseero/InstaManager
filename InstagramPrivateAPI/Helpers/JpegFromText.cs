f394fc1f8ff9d6e3ffd7cd609d9e668272a9caee
Kappaseero <casperkoponen2@gmail.com>
2022-10-30 20:29:59 +0200

    Class to convert text to images

---
+++ InstagramPrivateAPI/Helpers/JpegFromText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Helpers
+{
+    internal static class JpegFromText
+    {
+        public static string MakeJpeg(string text, string backgroundImage)
+        {
+            return string.Empty;
+        }
+    }
+}
