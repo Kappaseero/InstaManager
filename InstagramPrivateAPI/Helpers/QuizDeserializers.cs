b96e88c0e03c74c78cd0b631be2e93daa7daefde
Kappaseero <casperkoponen2@gmail.com>
2022-10-30 20:30:52 +0200

    Class to deserialize quiz api calls

---
+++ InstagramPrivateAPI/Helpers/QuizDeserializers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramPrivateAPI.Models;
+
+namespace InstagramPrivateAPI.Helpers
+{
+    internal static class QuizDeserializers
+    {
+        internal static QuestionAnswerModel OpenTDBDeserializer(QuestionAnswerModel model, HttpResponseMessage httpResponse)
+        {
+
+
+            return model;
+        }
+    }
+}
