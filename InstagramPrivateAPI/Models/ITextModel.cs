6a3252c16f55ba6bd2e30d85356af168af10f464
Kappaseero <casperkoponen2@gmail.com>
2022-11-08 18:45:45 +0200

    A text model to contain text rgba in bytes and font. Interface for it.

---
+++ InstagramPrivateAPI/Models/ITextModel.cs
@@ -0,0 +1,12 @@
+﻿namespace InstagramPrivateAPI.Models
+{
+    internal interface ITextModel
+    {
+        byte Alpha { get; set; }
+        byte Blue { get; set; }
+        string FontName { get; set; }
+        int FontSize { get; set; }
+        byte Green { get; set; }
+        byte Red { get; set; }
+    }
+}
---
+++ InstagramPrivateAPI/Models/TextModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Models
+{
+    internal class TextModel : ITextModel
+    {
+        public TextModel(string fontName = "Arial", int fontSize = 100, byte red = 0, byte green = 0, byte blue = 0, byte alpha = 255)
+        {
+            FontName = fontName;
+            FontSize = fontSize;
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public string FontName { get; set; }
+        public int FontSize { get; set; }
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+    }
+}
