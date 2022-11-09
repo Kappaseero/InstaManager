ab809e4f528e42e729049faa6c2b16881568d096
Kappaseero <casperkoponen2@gmail.com>
2022-11-09 11:38:45 +0200

    Framework for the deserializer but exact names have to be checked from the api response

--- InstagramPrivateAPI/Helpers/QuizDeserializers.cs
+++ InstagramPrivateAPI/Helpers/QuizDeserializers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 using InstagramPrivateAPI.Models;
 
 namespace InstagramPrivateAPI.Helpers
@@ -11,9 +12,17 @@ namespace InstagramPrivateAPI.Helpers
     {
         internal static IQuestionAnswerModel OpenTDBDeserializer(IQuestionAnswerModel model, HttpResponseMessage httpResponse)
         {
-
-
+            OpenTDBModel? midresult = JsonSerializer.Deserialize<OpenTDBModel>(httpResponse.Content.ToString());
+            model.Answer = midresult.CorrectAnswer;
+            model.WrongAnswers = midresult.WrongAnswers;
+            model.Question = midresult.Question;
             return model;
+        }
+        private class OpenTDBModel
+        {
+            public string WrongAnswers { get; set; } = string.Empty;
+            public string CorrectAnswer { get; set; } = string.Empty;
+            public string Question { get; set; } = string.Empty;
         }
     }
 }
