d8494e86dff2a47ab5777c08db76e9bdb42f56dd
Kappaseero <casperkoponen2@gmail.com>
2022-11-11 16:58:19 +0200

    Json deserializers moved to a new folder so its neater. A new abstract class for all quiz deserializers

---
+++ InstagramPrivateAPI/Helpers/Deserializers/OpenTDBQuizDeserializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using InstagramPrivateAPI.Models;
+using InstagramPrivateAPI.Helpers.Deserializers;
+
+namespace InstagramPrivateAPI.Helpers
+{
+    internal class OpenTDBQuizDeserializer : QuizDeserializer
+    {
+
+        internal override IQuestionAnswerModel? Deserialize (IQuestionAnswerModel model, HttpResponseMessage httpResponse)
+        {
+            ApiSpecificClass? midResult = new();
+            string? responseMessage = httpResponse.Content.ToString();
+            if (responseMessage == null)
+            {
+                return null;
+            }
+
+            midResult = JsonSerializer.Deserialize<ApiSpecificClass>(responseMessage);
+            if(midResult == null)
+            {
+                return null;
+            }
+
+            model.Answer = midResult.correct_answer;
+            model.WrongAnswers = midResult.incorrect_answers;
+            model.Question = midResult.question;
+            return model;
+        }
+        //used in the middle so the deserializer understands the names
+        private class ApiSpecificClass
+        {
+            public string incorrect_answers { get; set; } = string.Empty;
+            public string correct_answer { get; set; } = string.Empty;
+            public string question { get; set; } = string.Empty;
+        }
+    }
+}
---
+++ InstagramPrivateAPI/Helpers/Deserializers/QuizDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using InstagramPrivateAPI.Models;
+
+namespace InstagramPrivateAPI.Helpers.Deserializers
+{
+    internal abstract class QuizDeserializer
+    {
+        internal abstract IQuestionAnswerModel? Deserialize(IQuestionAnswerModel model, HttpResponseMessage responseMessage);
+
+        private abstract class ApiSpecificClass
+        {
+
+        }
+    }
+}
