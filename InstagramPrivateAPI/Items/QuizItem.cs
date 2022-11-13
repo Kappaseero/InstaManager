63ded62323df2af95633c873b707249da47f5c53
Kappaseero <casperkoponen2@gmail.com>
2022-11-13 19:04:51 +0200

    Alot of additions to QuizItem

--- InstagramPrivateAPI/Items/QuizItem.cs
+++ InstagramPrivateAPI/Items/QuizItem.cs
@@ -8,27 +8,58 @@ using InstagramPrivateAPI.Helpers;
 using InstagramPrivateAPI.InstagramInteracter;
 using InstagramApiSharp.API;
 using InstagramPrivateAPI.Models;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Classes;
+using InstagramPrivateAPI.InstagramInteracter.Posts;
+using InstagramPrivateAPI.Helpers.Captions;
 
 namespace InstagramPrivateAPI.Items
 {
     // the class that wraps everything together for a quiz
     internal class QuizItem
     {
-        public QuizItem(IGetQuiz getQuiz, IQuestionAnswerModel questionAnswer, ITextModel textModel, IInstaApi instaApi, Uri background)
+        public QuizItem(IGetQuiz getQuiz, IQuestionAnswerModel questionAnswer, ITextModel textModel, IInstaApi instaApi, Uri background, InstaImageUpload instaImage, TextToImageBase textToImage)
         {
+            TextToImage = textToImage;
             GetQuiz = getQuiz;
             QuestionAnswer = questionAnswer;
             TextModel = textModel;
             InstaApiInstance = instaApi;
             Background = background;
+            FinalImage = instaImage;
+            ReadyUpImage(FinalImage, textToImage);
+            Caption = QuizCaption.MakeCaption(questionAnswer);
         }
 
+        public InstaImageUpload FinalImage { get; set; }
+        public TextToImageBase TextToImage { get; set; }
         public IGetQuiz GetQuiz { get; set; }
         public IQuestionAnswerModel QuestionAnswer { get; set; }
         public Uri Background { get; set; }
         public ITextModel TextModel { get; set; }
         public IInstaApi InstaApiInstance { get; set; }
-        public bool HasBeenAnswered { get; set; }
+        public IResult<InstaMedia>? PostResult { get; set; }
+        public string Caption { get; set; }
+        public ResultInfo? PostInfo { get; set; }
 
+
+
+
+
+        public async Task Post()
+        {
+
+            PostResult = await ImagePoster.Post(InstaApiInstance, FinalImage, Caption);
+            PostInfo = PostResult.Info;
+        }
+
+        private void ReadyUpImage(InstaImageUpload instaImage, TextToImageBase textToImage)
+        {
+            instaImage.Height = 1080;
+            instaImage.Width = 1080;
+            instaImage.ImageBytes = textToImage.CreateImage();
+        }
+
+        
     }
 }
