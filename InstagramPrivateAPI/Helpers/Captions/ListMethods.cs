1e257b9e43ae4cddce794e8a8877dbc8c15c2a3f
Kappaseero <casperkoponen2@gmail.com>
2022-11-13 21:02:33 +0200

    A method to construct a quiz caption and a method to scramble a list for scrambling the right and wrong answers

---
+++ InstagramPrivateAPI/Helpers/Captions/ListMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Helpers.Captions
+{
+    internal static class ListMethods
+    {
+        public static List<string> ScrambleList(List<string> answersList)
+        {
+            Random rand = new Random();
+            List<string> scrambledAnswersList = new List<string>();
+
+            foreach (var answer in answersList)
+            {
+                scrambledAnswersList.Insert(rand.Next(0, scrambledAnswersList.Count + 1), answer);
+            }
+
+            return scrambledAnswersList;
+        }
+    }
+}
--- InstagramPrivateAPI/Helpers/Captions/QuizCaption.cs
+++ InstagramPrivateAPI/Helpers/Captions/QuizCaption.cs
@@ -9,9 +9,30 @@ namespace InstagramPrivateAPI.Helpers.Captions
 {
     internal static class QuizCaption
     {
-        public static string MakeCaption(IQuestionAnswerModel questionAnswerModel)
+        public static string MakeCaption(IQuestionAnswerModel questionAnswerModel, DateTime dateTime, out int rightAnswerNumber)
         {
-            return
+            List<string> answersList = questionAnswerModel.WrongAnswers.ToList();
+            answersList.Add(questionAnswerModel.Answer);
+            List<string> scrambledAnswersList = ListMethods.ScrambleList(answersList);
+
+            //+1 because count below starts from 1
+            rightAnswerNumber = scrambledAnswersList.IndexOf(questionAnswerModel.Answer) + 1;
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(dateTime.Date + ".");
+            stringBuilder.Append("Possible answers are:");
+            int count = 1;
+            foreach(string answer in scrambledAnswersList)
+            {
+                stringBuilder.Append($" {count}:{answer}.");
+                count++;
+            }
+            stringBuilder.Append(" Type the number of your answer as a comment.");
+
+            return stringBuilder.ToString();
         }
+
     }
 }
