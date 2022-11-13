fe18113777b97fb622216721f7a9748f0687a651
Kappaseero <casperkoponen2@gmail.com>
2022-11-13 18:52:14 +0200

    A static class to construct a understandable quiz caption with right and wrongs answers from a questionmodel

---
+++ InstagramPrivateAPI/Helpers/Captions/QuizCaption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramPrivateAPI.Models;
+
+namespace InstagramPrivateAPI.Helpers.Captions
+{
+    internal static class QuizCaption
+    {
+        public static string MakeCaption(IQuestionAnswerModel questionAnswerModel)
+        {
+            return
+        }
+    }
+}
