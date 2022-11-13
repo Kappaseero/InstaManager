090a6a2919c62938afeab9293b9a3f04884a455a
Kappaseero <casperkoponen2@gmail.com>
2022-11-13 18:48:00 +0200

    Deleted empty and useless for now classes and added a static class for posting images that can be used for all 1 photo posts

---
+++ InstagramPrivateAPI/InstagramInteracter/Posts/ImagePoster.cs
@@ -0,0 +1,24 @@
+﻿using InstagramApiSharp.Classes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramPrivateAPI.ExternalAPIAccess;
+using InstagramPrivateAPI.Helpers;
+using InstagramPrivateAPI.InstagramInteracter;
+using InstagramApiSharp.API;
+using InstagramPrivateAPI.Models;
+using InstagramApiSharp.Classes;
+
+namespace InstagramPrivateAPI.InstagramInteracter.Posts
+{
+    internal static class ImagePoster
+    {
+       public static async Task<IResult<InstaMedia>> Post(IInstaApi instaApi, InstaImageUpload instaImage, string caption)
+        {
+            var post = await instaApi.MediaProcessor.UploadPhotoAsync(instaImage, caption);
+            return post;
+        }
+    }
+}
