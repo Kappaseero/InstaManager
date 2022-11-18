5781155628bedb1bfd1059901674335149b304ad
Kappaseero <casperkoponen2@gmail.com>
2022-11-18 15:58:36 +0200

    Now takes a quizsettings and takes most objects from there. A method for posting both guessed and unguessed quiz

--- InstagramPrivateAPI/Items/QuizItem.cs
+++ InstagramPrivateAPI/Items/QuizItem.cs
@@ -12,60 +12,116 @@ using InstagramApiSharp.Classes.Models;
 using InstagramApiSharp.Classes;
 using InstagramPrivateAPI.InstagramInteracter.Posts;
 using InstagramPrivateAPI.Helpers.Captions;
+using InstagramPrivateAPI.Helpers.Deserializers;
 
 namespace InstagramPrivateAPI.Items
 {
     // the class that wraps everything together for a quiz
-    internal class QuizItem
+    public class QuizItem
     {
-        public QuizItem(IGetQuiz getQuiz, IQuestionAnswerModel questionAnswer, ITextModel textModel, IInstaApi instaApi, Uri background, InstaImageUpload instaImage, TextToImageBase textToImage)
-        {
-            TextToImage = textToImage;
-            GetQuiz = getQuiz;
-            QuestionAnswer = questionAnswer;
-            TextModel = textModel;
-            InstaApiInstance = instaApi;
-            Background = background;
-            FinalImage = instaImage;
-            ReadyUpImage(FinalImage, textToImage);
-            DateTimeCreated = DateTime.Now;
+        //public QuizItem(IGetQuiz getQuiz, IQuestionAnswerModel questionAnswer, ITextModel textModel, IInstaApi instaApi, InstaImageUpload instaImage, TextToImageBase textToImage, DateTime dateTimeNextQuiz)
+        //{
+        //    TextToImage = textToImage;
+        //    GetQuiz = getQuiz;
+        //    QuestionAnswer = questionAnswer;
+        //    TextModel = textModel;
+        //    InstaApiInstance = instaApi;
+        //    Background = background;
+        //    FinalImage = instaImage;
+        //    ReadyUpImage(FinalImage, textToImage);
+        //    DateTimeCreated = DateTime.Now;
+        //    DateTimeNextQuiz = dateTimeNextQuiz;
 
-            int rightAnswer;
-            Caption = QuizCaption.MakeCaption(questionAnswer, DateTimeCreated, out rightAnswer);
-            RightAnswer = rightAnswer;
+        //    int rightAnswer;
+        //    Caption = QuizCaption.UnguessedCaption(QuestionAnswer, DateTimeCreated, out rightAnswer);
+        //    RightAnswer = rightAnswer;
+        //    EndedCaption = QuizCaption.GuessedCaption(QuestionAnswer, DateTimeCreated, DateTimeNextQuiz);
+
+
+
+        //}
+
+        public QuizItem(QuizSettingsBase quizSettings)
+        {
+            DateTimeNextQuiz = quizSettings.DateTimeNextQuiz;
+            Image = quizSettings.Image;
+            TextToImage = quizSettings.TextToImage;
+            GetQuiz = quizSettings.GetQuiz;
+            QuestionAnswer = quizSettings.QuestionAnswer;
+            UnguessedBackground = quizSettings.UnguessedBackground;
+            GuessedBackground = quizSettings.GuessedBackground;
+            TextModel = quizSettings.TextModel;
+            InstaApiInstance = quizSettings.InstaApiInstance;
+            Deserializer = quizSettings.Deserializer;
         }
 
-        public DateTime DateTimeCreated { get; set; }
-        public InstaImageUpload FinalImage { get; set; }
-        public TextToImageBase TextToImage { get; set; }
-        public IGetQuiz GetQuiz { get; set; }
-        public IQuestionAnswerModel QuestionAnswer { get; set; }
-        public Uri Background { get; set; }
-        public ITextModel TextModel { get; set; }
-        public IInstaApi InstaApiInstance { get; set; }
-        public IResult<InstaMedia>? PostResult { get; set; }
-        public string Caption { get; set; }
-        public ResultInfo? PostInfo { get; set; }
-        public int RightAnswer { get; set; }
 
 
 
+        private DateTime DateTimeCreated { get; set; } = DateTime.Now;
+        private  DateTime DateTimeNextQuiz { get; set; }
+        private  InstaImageUpload Image { get; set; }
+        private  TextToImageBase TextToImage { get; set; }
+        private  IGetQuiz GetQuiz { get; set; }
+        private  IQuestionAnswerModel QuestionAnswer { get; set; }
+        private  Uri UnguessedBackground { get; set; }
+        private  Uri GuessedBackground { get; set; }
+        private  ITextModel TextModel { get; set; }
+        private  IInstaApi InstaApiInstance { get; set; }
+        private  QuizDeserializer Deserializer { get; set; }
 
 
-        public async Task Post()
-        {
+        private  IResult<InstaMedia>? PostResult { get; set; }
+        private  ResultInfo? PostInfo { get; set; }
+        private  int RightAnswer { get; set; }
 
-            PostResult = await ImagePoster.Post(InstaApiInstance, FinalImage, Caption);
-            PostInfo = PostResult.Info;
-        }
-
-        private void ReadyUpImage(InstaImageUpload instaImage, TextToImageBase textToImage)
-        {
-            instaImage.Height = 1080;
-            instaImage.Width = 1080;
-            instaImage.ImageBytes = textToImage.CreateImage();
-        }
 
         
+
+
+        public async Task PostUnguessed()
+        {
+            //get a http response from api with GetQuiz and deserializing it
+            try
+            {
+                var response = await GetQuiz.GetDataAsync();
+                QuestionAnswer = Deserializer.Deserialize(QuestionAnswer, response);
+                Image.ImageBytes = TextToImage.CreateImage(UnguessedBackground, TextModel, QuestionAnswer.Question);
+
+                string caption = QuizCaption.UnguessedCaption(QuestionAnswer, DateTimeCreated, out int rightAnswer);
+                RightAnswer = rightAnswer;
+                PostResult = await ImagePoster.Post(InstaApiInstance, Image, caption);
+                PostInfo = PostResult.Info;
+            }
+            catch (Exception ex)
+            {
+                NotifyOfException(ex);
+            }
+            
+        }
+
+        public async Task PostGuessed()
+        {
+            try
+            {
+                Image.ImageBytes = TextToImage.CreateImage(UnguessedBackground, TextModel, QuestionAnswer.Question);
+
+                string caption = QuizCaption.GuessedCaption(QuestionAnswer, DateTimeCreated, DateTimeNextQuiz);
+                PostResult = await ImagePoster.Post(InstaApiInstance, Image, caption);
+                PostInfo = PostResult.Info;
+            }
+            catch (Exception ex)
+            {
+                NotifyOfException(ex);
+            }
+        }
+
+       
+
+        private void NotifyOfException(Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            Console.WriteLine(exception.Source);
+        }
     }
 }
