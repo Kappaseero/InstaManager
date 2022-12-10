a8bedef60d894f3bc765202361f35f9b91a4c192
Kappaseero <casperkoponen2@gmail.com>
2022-12-10 19:36:25 +0200

    Web api project deleted and added a windows forms project for ui

---
+++ InstaManagerUI/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace InstaManagerUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.accountPage = new System.Windows.Forms.TabPage();
+            this.quizPage = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.accountPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.accountPage);
+            this.tabControl1.Controls.Add(this.quizPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(691, 398);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // accountPage
+            // 
+            this.accountPage.Controls.Add(this.statusLabel);
+            this.accountPage.Controls.Add(this.passwordLabel);
+            this.accountPage.Controls.Add(this.usernameLabel);
+            this.accountPage.Controls.Add(this.textBox2);
+            this.accountPage.Controls.Add(this.textBox1);
+            this.accountPage.Controls.Add(this.button1);
+            this.accountPage.Location = new System.Drawing.Point(4, 24);
+            this.accountPage.Name = "accountPage";
+            this.accountPage.Padding = new System.Windows.Forms.Padding(3);
+            this.accountPage.Size = new System.Drawing.Size(683, 370);
+            this.accountPage.TabIndex = 0;
+            this.accountPage.Text = "Account";
+            this.accountPage.UseVisualStyleBackColor = true;
+            // 
+            // quizPage
+            // 
+            this.quizPage.Location = new System.Drawing.Point(4, 24);
+            this.quizPage.Name = "quizPage";
+            this.quizPage.Padding = new System.Windows.Forms.Padding(3);
+            this.quizPage.Size = new System.Drawing.Size(683, 370);
+            this.quizPage.TabIndex = 1;
+            this.quizPage.Text = "Quiz";
+            this.quizPage.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 245);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(222, 161);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(60, 15);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(222, 227);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(57, 15);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.statusLabel.ForeColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(222, 24);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(171, 32);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.Text = "Not signed in";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 398);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.accountPage.ResumeLayout(false);
+            this.accountPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage accountPage;
+        private TabPage quizPage;
+        private Label statusLabel;
+        private Label passwordLabel;
+        private Label usernameLabel;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
