30d1dada60d1c239dba00d5c647e9290cb7ce0a3
Kappaseero <casperkoponen2@gmail.com>
2022-12-12 16:13:14 +0200

    Enums for login attempt feedback

---
+++ InstagramPrivateAPI/Enums/LoginEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramPrivateAPI.Enums
+{
+    public enum LoginEnum
+    {
+        Success,
+        Challenged,
+        Unsuccessful
+    }
+}
